83eb5367dec23fd4edf198df523aef3e1f3dfd8f
Andre Monteiro <andrefmonteiro@outlook.com>
2024-03-06 10:46:47 +0000

    delDel resolution is added

---
+++ warmup-1/delDel.js
@@ -0,0 +1,18 @@
+/*
+     Given a string, if the string "del" appears starting at index 1, return a string where that "del" has been deleted. Otherwise, return the string unchanged.
+
+     Examples
+
+     delDel('adelbc') → abc
+     delDel('adelHello') → aHello
+     delDel('abcdel') → abcdel
+*/
+
+function delDel(str) {
+
+     if (str.length >= 4 && str.substring(1, 4).toLowerCase() === "del") {
+
+          return str.charAt(0) + str.substring(4);
+     }
+     return str;
+}
