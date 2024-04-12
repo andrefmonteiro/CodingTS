a851924972aed8a5743ca391bc5403f1f1d34c85
Andre Monteiro <andrefmonteiro@outlook.com>
2024-04-12 12:22:20 +0100

    intMax resolution added

---
+++ warmup-1/intMax.ts
@@ -0,0 +1,16 @@
+/*
+     Warmup-1 -- intMax
+     Given three int values, a b c, return the largest.
+
+     Examples
+
+     intMax(1, 2, 3) → 3
+     intMax(1, 3, 2) → 3
+     intMax(3, 2, 1) → 3
+*/
+
+function intMax(a: number, b: number, c: number): number {
+     
+     return Math.max(a, b, c);
+     // Java's Math.max() only takes in 2 params :)
+}
