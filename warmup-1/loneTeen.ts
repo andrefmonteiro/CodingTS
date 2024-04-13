1a95a8b451181140ed69ac1f54e293f3a42a74e3
Andre Monteiro <andrefmonteiro@outlook.com>
2024-04-13 20:11:39 +0100

    loneTeen resolution is added

---
+++ warmup-1/loneTeen.ts
@@ -0,0 +1,25 @@
+/*
+Warmup-1 -- loneTeen
+We'll say that a number is 'teen' if it is in the range 13..19 inclusive. Given 2 int values, return true if one or the other is teen, but not both.
+
+Examples
+
+loneTeen(13, 99) → true
+loneTeen(21, 19) → true
+loneTeen(13, 13) → false
+*/
+
+function loneTeen(num1: number, num2: number) {
+	// one has to be true
+	// the other has to be false
+
+	function isTeen(n: number): boolean {
+		return n >= 13 && n <= 19;
+	}
+
+	if (isTeen(num1)) {
+		return !isTeen(num2);
+	}
+
+	return isTeen(num2);
+}
