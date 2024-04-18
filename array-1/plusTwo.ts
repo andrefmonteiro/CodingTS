165e190968f669290f8f29f58b7e237ebddae8b7
Andre Monteiro <andrefmonteiro@outlook.com>
2024-04-18 10:02:55 +0100

    plusTwo solution added

---
+++ array-1/plusTwo.ts
@@ -0,0 +1,20 @@
+/*
+Array-1 -- plusTwo
+Given 2 int arrays, each length 2, return a new array length 4 containing all their elements.
+
+Examples
+
+plusTwo([1, 2], [3, 4]) → 1,2,3,4
+plusTwo([4, 4], [2, 2]) → 4,4,2,2
+plusTwo([9, 2], [3, 4]) → 9,2,3,4
+*/
+
+function plusTwo(array1: number[], array2: number[]): number[] {
+     
+     const finalArray: number[] = [];
+
+     finalArray.push(...array1);
+     finalArray.push(...array2);
+
+     return finalArray;
+}
