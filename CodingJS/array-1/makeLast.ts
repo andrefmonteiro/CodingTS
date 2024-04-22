ba8c21652d76f2d681991e6a62d8f2c8d67888ff
Andre Monteiro <andrefmonteiro@outlook.com>
2024-04-22 16:20:24 +0100

    folder structure changed

---
+++ CodingJS/array-1/makeLast.ts
@@ -0,0 +1,19 @@
+/*
+Array-1 -- makeLast
+Given an int array, return a new array with double the length where its last element is the same as the original array, and all the other elements are 0. The original array will be length 1 or more.
+
+Examples
+
+makeLast([4, 5, 6]) → 0,0,0,0,0,6
+makeLast([1, 2]) → 0,0,0,2
+makeLast([3]) → 0,3
+*/
+
+function makeLast(array: number[]): number[] {
+     
+     // get original length
+     // get last element
+     // repeat 0: double original length - 1
+
+          
+}
